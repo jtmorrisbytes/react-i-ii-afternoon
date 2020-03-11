e70b084ef7e0aca76019167d0c981ec7a306c090
= <jthecybertinkerer@gmail.com>
2020-03-11 15:09:43 -0500

    App.js: added a function to handle requests for the next student

--- student-info/src/App.js
+++ student-info/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
+import data from "./data";
+
 import StudentInfo from "./components/StudentInfo";
+
 function Navigation(props) {
   return (
     <nav id="Navigation">
@@ -8,14 +11,41 @@ function Navigation(props) {
     </nav>
   );
 }
+export default class App extends React.Component {
+  state = {
+    studentData: [...data],
+    currentStudent: 0
+  };
 
-function App() {
-  return (
-    <div ID="App">
-      <Navigation />
-      <StudentInfo />
-    </div>
-  );
+  nextStudent = () => {
+    // I know im being paranoid here, but just in case...
+    if (this.state.currentStudent > this.state.studentData.length - 1) {
+      this.setState({ currentStudent: this.state.studentData.length - 1 });
+      return;
+    } else if (
+      this.state.currentStudent ===
+      this.state.studentData.length - 1
+    ) {
+      // Ideally here I would display some sort of feedback, like disabling
+      // the next button or a message or modal of some kind.
+      // but the current iteration of this function will do nothing
+      console.info(
+        "App nextStudent(): This is as far as we can go... doing nothing"
+      );
+      return;
+    } else {
+      this.setState({ currentStudent: this.state.currentStudent + 1 });
+    }
+  };
+  previousStudent = () => {};
+  goToStudent = idNum => {};
+
+  render() {
+    return (
+      <div ID="App">
+        <Navigation />
+        <StudentInfo />
+      </div>
+    );
+  }
 }
-
-export default App;
