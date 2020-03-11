b44cd5e1e35b53fa4f12dfe7264c78b91b90ac81
= <jthecybertinkerer@gmail.com>
2020-03-11 16:05:23 -0500

    implemented previousStudent function, restructured nextStudent

--- student-info/src/App.js
+++ student-info/src/App.js
@@ -21,35 +21,49 @@ export default class App extends React.Component {
     // I know im being paranoid here, but just in case...
     if (this.state.currentStudent > this.state.studentData.length - 1) {
       this.setState({ currentStudent: this.state.studentData.length - 1 });
+      console.info(
+        "somehow the current student was set beyond the index of the list. resetting..."
+      );
       return;
-    } else if (
-      this.state.currentStudent ===
-      this.state.studentData.length - 1
-    ) {
+    } else if (this.state.currentStudent < this.state.studentData.length - 1) {
+      this.setState({ currentStudent: this.state.currentStudent + 1 });
+    } else {
       // Ideally here I would display some sort of feedback, like disabling
       // the next button or a message or modal of some kind.
       // but the current iteration of this function will do nothing
       console.info(
         "App nextStudent(): This is as far as we can go... doing nothing"
       );
-      return;
-    } else {
-      this.setState({ currentStudent: this.state.currentStudent + 1 });
     }
   };
-  previousStudent = () => {};
-  goToStudent = idNum => {};
+  previousStudent = () => {
+    if (this.state.currentPosition < 0) {
+      console.log("somehow the current student was set negative... resetting");
+      this.setState({ currentStudent: 0 });
+    } else if (this.state.currentStudent > 0) {
+      this.setState({ currentStudent: this.state.currentStudent - 1 });
+    } else {
+      console.info(
+        "App previousStudent(): this is the first student, cant go any further..."
+      );
+    }
+  };
+  goToFirstStudent = () => {
+    this.setState({ currentStudent: 0 });
+  };
 
   render() {
     let currentPosition =
       this.state.currentStudent + 1 + "/" + this.state.studentData.length;
     return (
-      <div ID="App">
+      <div id="App">
         <Navigation />
         <StudentInfo
           nextStudent={this.nextStudent}
           previousStudent={this.previousStudent}
           student={this.state.studentData[this.state.currentStudent]}
+          currentStudent={this.state.currentStudent}
+          numStudents={this.state.studentData.length}
           currentPosition={currentPosition}
         />
       </div>
