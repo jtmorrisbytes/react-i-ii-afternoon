0f8b06304c77aea085c5934dcfda42d6320a2d0c
= <jthecybertinkerer@gmail.com>
2020-03-11 15:42:07 -0500

    StudentInfoCard: now rendering the student name

--- student-info/src/components/StudentInfoCard.js
+++ student-info/src/components/StudentInfoCard.js
@@ -3,25 +3,23 @@ import React from "react";
 function StudentOverviewItem(props) {
   return (
     <div className="studentOverviewItem">
-      <span className="label">{props.label}</span>
+      <span className="label">{props.label + " "}</span>
       <span className="text">{props.text}</span>
     </div>
   );
 }
 export default function StudentInfoCard(props) {
   // hometown displays nothing if props are undefined
-  let hometown =
-    (props.city || "") +
-    (props.city && props.country ? "," : "") +
-    (props.country || "");
+  let { city, country, employer, title, name } = props.student;
+  let hometown = (city || "") + (city && country ? ", " : "") + (country || "");
   return (
     <div className="StudentInfoCard">
-      <span className="listPosition">{props.listPosition || "0/0"}</span>
-      <h1 className="studentHeading">Student Card</h1>
+      <span className="listPosition">{props.currentPosition || "0/0"}</span>
+      <h1 className="studentHeading">{name.first + " " + name.last}</h1>
       <div className="studentOverview">
         <StudentOverviewItem label="From:" text={hometown} />
-        <StudentOverviewItem label="Job Title:" text={props.title} />
-        <StudentOverviewItem label="Employer" text={props.employer} />
+        <StudentOverviewItem label="Job Title:" text={title} />
+        <StudentOverviewItem label="Employer:" text={employer} />
       </div>
       <div className="studentFavorites"></div>
     </div>
