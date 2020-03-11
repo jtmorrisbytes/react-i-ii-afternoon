9fdcdd1298a20c5d79fc4db95fdc16814a75d856
= <jthecybertinkerer@gmail.com>
2020-03-11 14:41:26 -0500

    seperated components into thier own functions

--- student-info/src/App.js
+++ student-info/src/App.js
@@ -1,18 +1,62 @@
 import React from "react";
 import "./App.css";
+function Navigation(props) {
+  return (
+    <nav id="Navigation">
+      <a href="/#/home">Home</a>
+    </nav>
+  );
+}
+function StudentInfo(props) {
+  return (
+    <div id="StudentInfo">
+      <StudentInfoCard />
+      <div id="StudentInfoControls">
+        <button>Previous</button>
+        <div className="StudentInfoActionGroup">
+          <button>Edit</button>
+          <button>Delete</button>
+          <button>New</button>
+        </div>
+        <button>Next</button>
+      </div>
+    </div>
+  );
+}
+
+function StudentOverviewItem(props) {
+  return (
+    <div className="studentOverviewItem">
+      <span className="label">{props.label}</span>
+      <span className="text">{props.text}</span>
+    </div>
+  );
+}
+
+function StudentInfoCard(props) {
+  // hometown displays nothing if props are undefined
+  let hometown =
+    (props.city || "") +
+    (props.city && props.country ? "," : "") +
+    (props.country || "");
+  return (
+    <div className="StudentInfoCard">
+      <h1 className="studentHeading">Student Card</h1>
+      <div className="studentOverview">
+        <StudentOverviewItem label="From:" text={hometown} />
+        <StudentOverviewItem label="Job Title:" text={props.title} />
+        <StudentOverviewItem label="Employer" text={props.employer} />
+      </div>
+      <div className="studentFavorites"></div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div ID="App">
-      <nav id="Navigation">
-        <a href="/#/home">Home</a>
-      </nav>
-      <div id="StudentInfoController">
-        <div className="StudentInfoCard">
-          <h1>Student Card</h1>
-        </div>
-        <div id="StudentInfoControls"></div>
-      </div>
+      <Navigation />
+      <StudentInfo />
     </div>
   );
 }
